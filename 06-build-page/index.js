7d1d1f5372a122a7bd9d8379226432cfc9524d36
Aleksandr Bet <alex.van.bet@gmail.com>
2022-05-25 23:52:37 +0300

    fix: fix 6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const { rm, mkdir } = require('fs/promises');
+const { readdir, mkdir, rm, readFile } = require('fs/promises');
 let currentFolder = (path.join(__dirname)); /* текущая папка */
 let projectDist = (path.join(__dirname, 'project-dist')); /* папка проекта */
 let assets = (path.join(__dirname, 'assets')); /* папка с картинками */
@@ -33,11 +33,7 @@ function createStyle() {
       let extension = path.parse(files[i].name).ext;
       if (extension === '.css') {
 
-        
-
         addCss(files[i].name);
-
-      
 
       }
 
@@ -87,92 +83,67 @@ function copyFolderImg(checkpWay, pasteWay) {
 
 
 /* создание  index.html*/
-function addhtml() {
-  fs.readdir(components, {withFileTypes: true}, (err, files) => {
-    if (err) {throw err;}
-
-    let temp = '';
-    fs.readFile(path.join(projectDist, 'index.html'), 'utf-8', (err, data) => {
-      if (err) {throw err;}
-
-      temp = data;
-    });
-
-    for (let i = 0; i < files.length; i++) {
-
-      let extension = path.parse(files[i].name).ext;
+async function addhtml() {
+  
+    let files = await readdir(components, {withFileTypes: true})
+    let temp = await readFile(path.join(projectDist, 'index.html'), 'utf-8')
+    for (const i of files) {
+     
+      let extension = path.parse(i.name).ext;
+      let name = path.parse(i.name).name;
+      let data =  await readFile(path.join(components, i.name), 'utf-8')
+      temp = temp.replace(`{{${name}}}`, data);
+      const paste = fs.createWriteStream(path.join(projectDist, 'index.html'))
+      
       if (extension === '.html') {
-        let name = path.parse(files[i].name).name;
-
-        fs.readFile(path.join(components, files[i].name), 'utf-8', (err, data) => {
-          if (err) {throw err;}
-
-          temp = temp.replace(`{{${name}}}`, data);
-          const paste = fs.createWriteStream(path.join(projectDist, 'index.html'), (err) => {
-            if (err) {throw err;}
-          });
-         
-          paste.write(temp);
-
-          fs.readFile(path.join(path.join(projectDist, 'index.html')), 'utf-8', (err, data) => {
-            if (err) {throw err;}
-
-            let flag = true;
-            while (flag === true) {
-
-              if (!data.includes(`{{${name}}}`)) {
-                flag = false;
-              }
-              
-            }
-
-          });
-
-        });
+        paste.write(temp);
       }
-
     }
-  });
-
 }
 
 
 /* создание папки project-dist, запуск сборки */
 async function createFolder() {
+  try {
 
-  await rm(projectDist, {recursive: true, force: true,}, (err) => {
-    if (err) {throw err;}
-  });
+    await rm(projectDist, {recursive: true, force: true,})
+    await mkdir(projectDist, {recursive: true, force: true,})
 
-  await mkdir(projectDist, (err) => {
-    if (err) {throw err;}
-  });
-
-
-
-  fs.readdir(currentFolder, {withFileTypes: true}, (err, files) => {
-    if (err) {throw err;}
-
-    fs.mkdir(assetsCopy, (err) => {
+    fs.readdir(currentFolder, {withFileTypes: true, force: true,}, (err, files) => {
       if (err) {throw err;}
-      copyFolderImg(assets, assetsCopy);
-    });
-
-    fs.writeFile(path.join(projectDist, 'style.css'), '', (err) => {
-      if (err) {throw err;}
-      createStyle();
+  
+      fs.mkdir(assetsCopy, (err) => {
+        if (err) {throw err;}
+        
+        copyFolderImg(assets, assetsCopy);
+        
+      });
+  
+      fs.writeFile(path.join(projectDist, 'style.css'), '', (err) => {
+        if (err) {throw err;}
+        
+        createStyle();
+      });
+  
     });
 
     fs.readFile(path.join(currentFolder, 'template.html'), (err, data) => {
       if (err) {throw err;}
       fs.writeFile(path.join(projectDist, 'index.html'), data, (err) => {
         if (err) {throw err;}
+       
         addhtml();
       });
     });
+  
+
+  }
+
+  catch {
+    console.log(err);
+  }
 
 
-  });
 
 }
 createFolder();
