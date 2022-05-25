64aead7682403562ce96bb9f6642ee429b526c18
Aleksandr Bet <alex.van.bet@gmail.com>
2022-05-26 00:44:21 +0300

    fix: fix 6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const { readdir, mkdir, rm, readFile } = require('fs/promises');
+const { readdir, mkdir, rm, readFile, writeFile } = require('fs/promises');
 let currentFolder = (path.join(__dirname)); /* текущая папка */
 let projectDist = (path.join(__dirname, 'project-dist')); /* папка проекта */
 let assets = (path.join(__dirname, 'assets')); /* папка с картинками */
@@ -92,13 +92,14 @@ async function addhtml() {
       let extension = path.parse(i.name).ext;
       let name = path.parse(i.name).name;
       let data =  await readFile(path.join(components, i.name), 'utf-8')
-      temp = temp.replace(`{{${name}}}`, data);
-      const paste = fs.createWriteStream(path.join(projectDist, 'index.html'))
       
       if (extension === '.html') {
-        paste.write(temp);
-      }
+        temp = await temp.replace(`{{${name}}}`, data);
+      }  
+
     }
+
+    await writeFile(path.join(projectDist, 'index.html'), temp, 'utf-8');
 }
 
 
@@ -109,22 +110,16 @@ async function createFolder() {
     await rm(projectDist, {recursive: true, force: true,})
     await mkdir(projectDist, {recursive: true, force: true,})
 
-    fs.readdir(currentFolder, {withFileTypes: true, force: true,}, (err, files) => {
+
+    fs.mkdir(assetsCopy, (err) => {
       if (err) {throw err;}
-  
-      fs.mkdir(assetsCopy, (err) => {
-        if (err) {throw err;}
-        
-        copyFolderImg(assets, assetsCopy);
-        
-      });
-  
-      fs.writeFile(path.join(projectDist, 'style.css'), '', (err) => {
-        if (err) {throw err;}
-        
-        createStyle();
-      });
-  
+      copyFolderImg(assets, assetsCopy);
+      
+    });
+
+    fs.writeFile(path.join(projectDist, 'style.css'), '', (err) => {
+      if (err) {throw err;}
+      createStyle();
     });
 
     fs.readFile(path.join(currentFolder, 'template.html'), (err, data) => {
@@ -142,7 +137,6 @@ async function createFolder() {
   catch {
     console.log(err);
   }
-
 
 
 }
